3d63b11f1075d05b5b9c82488937a314f243c64a
mohiqudraty <mohi.qudraty@gmail.com>
2026-09-18 20:10:45 +0600

    file path issues resoleve for searchBar.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import SearchBar from "../components/Searchbar";
+import SearchBar from "../components/SearchBar";
 import MovieCard from "../components/MovieCard";
 import MovieModal from "../components/MovieModal";
 import Loader from "../components/Loader";
